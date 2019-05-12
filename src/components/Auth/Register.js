e0648ce3eb6d561e80b9e34fb0ffc0d530ea447a
nimaOngchu <nima.ongchu@gmail.com>
2019-05-12 21:05:25 +1000

    register component added

---
+++ src/components/Auth/Register.js
@@ -0,0 +1,76 @@
+// eslint-disable-next-line jsx-a11y/href-no-hash
+import React, { Component } from "react";
+import {
+  Grid,
+  Form,
+  Button,
+  Message,
+  Header,
+  Segment,
+  Icon
+} from "semantic-ui-react";
+import { Link } from "react-router-dom";
+export class Register extends Component {
+  state = {};
+  handleChange = () => {};
+  render() {
+    return (
+      <Grid textAlign="center" verticalAlign="middle">
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as="h2" icon color="orange" textAlign="center">
+            <Icon name="puzzle piece" color="orange" />
+            Register for DevChat
+          </Header>
+          <Form size="large">
+            <Segment stacked>
+              <Form.Input
+                fluid
+                name="username"
+                icon="user"
+                iconPosition="left"
+                placeholder="Username"
+                onChange={this.handleChange}
+                type="text"
+              />
+              <Form.Input
+                fluid
+                name="email"
+                icon="mail"
+                iconPosition="left"
+                placeholder="Email Address"
+                onChange={this.handleChange}
+                type="email"
+              />
+              <Form.Input
+                fluid
+                name="password"
+                icon="lock"
+                iconPosition="left"
+                placeholder="Passowrd"
+                onChange={this.handleChange}
+                type="password"
+              />
+              <Form.Input
+                fluid
+                name="passwordConfimation"
+                icon="lock"
+                iconPosition="left"
+                placeholder="Confirm Passowrd"
+                onChange={this.handleChange}
+                type="password"
+              />
+              <Button color="orange" fluid size="large">
+                Submit
+              </Button>
+            </Segment>
+          </Form>
+          <Message>
+            Already a User ?<Link to="/login"> Login</Link>
+          </Message>
+        </Grid.Column>
+      </Grid>
+    );
+  }
+}
+
+export default Register;
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app'
+var firebaseConfig = {
+    apiKey: "AIzaSyAPMRdtvVrdMWgY-6lXXEMjC6AgDEroNK0",
+    authDomain: "awsomechat-8828f.firebaseapp.com",
+    databaseURL: "https://awsomechat-8828f.firebaseio.com",
+    projectId: "awsomechat-8828f",
+    storageBucket: "awsomechat-8828f.appspot.com",
+    messagingSenderId: "146340850676",
+    appId: "1:146340850676:web:5c77bdca14819407"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
